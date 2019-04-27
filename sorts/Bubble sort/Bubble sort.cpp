b83ae8edd539805a460e4aa57cc96c593f8e55c7
Maxim Shakura <43370239+M2000h@users.noreply.github.com>
2019-04-27 22:14:57 +0300

    Update Bubble sort.cpp

--- sorts/Bubble sort/Bubble sort.cpp
+++ sorts/Bubble sort/Bubble sort.cpp
@@ -1,43 +1,42 @@
+
 #include <iostream>
 
 using namespace std;
 
+void BubleSort(int *arr, int size) {
+	int temp;
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = 0; j < size - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
 int main()
 {
-    int *arr; 
-    int size; 
-    cout << "n = ";
-    cin >> size;
+	int *arr;
+	int size;
+	cout << "n = ";
+	cin >> size;
+	
+	arr = new int[size];
+	for (int i = 0; i < size; i++) {
+		cout << "arr[" << i << "] = ";
+		cin >> arr[i];
+	}
+	
+	
+	BubleSort(arr, size);
 
-    if (size <= 0) {
-        cerr << "Invalid size" << endl;
-        return 1;
-    }
 
-    arr = new int[size]; 
-    for (int i = 0; i < size; i++) {
-        cout << "arr[" << i << "] = ";
-        cin >> arr[i];
-    }
-
-    int temp; 
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // меняем элементы местами
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
-    delete [] arr;
-    
-    return 0;
+	//output
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	delete[] arr;
+	return 0;
 }
