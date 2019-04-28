2b2eb8071153729cf3cfbfaad0ea9f538ccb13c5
Maxim Shakura <43370239+M2000h@users.noreply.github.com>
2019-04-28 14:09:10 +0300

    Update Bubble sort.cpp

--- sorts/Bubble sort/Bubble sort.cpp
+++ sorts/Bubble sort/Bubble sort.cpp
@@ -1,42 +1,63 @@
-
 #include <iostream>
 
 using namespace std;
 
+
+
+///
+// Sort function
+///
 void BubleSort(int *arr, int size) {
-	int temp;
 	for (int i = 0; i < size - 1; i++) {
 		for (int j = 0; j < size - i - 1; j++) {
 			if (arr[j] > arr[j + 1]) {
-				temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
+				swap(arr[j], arr[j + 1]);
 			}
 		}
 	}
 }
+///
+// End of sort function
+///
 int main()
 {
+	///
+	// Exemple of usage
+	///
 	int *arr;
 	int size;
 	cout << "n = ";
 	cin >> size;
 	
 	arr = new int[size];
+
+
+	///
+	// Read array
+	///
 	for (int i = 0; i < size; i++) {
 		cout << "arr[" << i << "] = ";
 		cin >> arr[i];
 	}
 	
 	
+
+	///
+	// Calling a sort function
+	///
 	BubleSort(arr, size);
 
 
-	//output
+	///
+	// Output result
+	///
 	for (int i = 0; i < size; i++) {
 		cout << arr[i] << " ";
 	}
 	cout << endl;
 	delete[] arr;
 	return 0;
+	///
+	//  End of exemple of usage
+	///
 }
