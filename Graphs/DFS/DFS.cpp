c5b4b6ff38ba1a4d9460ce3c525753460dcf4839
Maxim Shakura <43370239+M2000h@users.noreply.github.com>
2019-05-15 21:12:37 +0300

    Update DFS.cpp

--- Graphs/DFS/DFS.cpp
+++ Graphs/DFS/DFS.cpp
@@ -1,7 +1,4 @@
-#include <iostream> 
-#include <string>
-#include <vector>
-#include <algorithm>
+#include <iostream>
 using namespace std;
 
 int Graph[100][100];
@@ -11,7 +8,6 @@ int finish;
 
 void DFS(int v, int from, int size, int deep) {
 	Mark[v] = 1;
-	
 	Way[deep] = v;
 	if (v == finish)
 	{
@@ -21,10 +17,9 @@ void DFS(int v, int from, int size, int deep) {
 		return;
 	}
 	for (int i = 1; i <= size; ++i)
-	{
 		if (Mark[i] == 0 && Graph[v][i])
 			DFS(i, v, size,deep+1);
-	}
+	
 }
 int main(void)
 {
